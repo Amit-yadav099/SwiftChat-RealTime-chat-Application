01c06ba2bd55ae20a504562d80d46c44e0cf81b5
Amit Yadav <kartiktroy00001@gmail.com>
2025-08-23 23:03:59 +0530

    login, logout & update profile added

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import { connectDB } from "./lib/db.js";
 
@@ -10,8 +11,8 @@ dotenv.config();
 const PORT = process.env.PORT;
 
 app.use(express.json());
-
 app.use(cookieParser());
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
